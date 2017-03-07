db3cca645bd7450257c1bce6e92f2af305c1c135
Kim JeongHyun <ashcroft147@gmail.com>
2017-03-08 01:24:25 +0900

    Add for react

---
+++ src/index.js
@@ -0,0 +1,21 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import App from './App';
+import './index.css';
+import * as firebase from 'firebase';
+
+// Initialize Firebase
+var config = {
+    apiKey: "AIzaSyBh95mLLrG7CCD5Ts810guFuP-HMXVwuss",
+    authDomain: "portpolio-16967.firebaseapp.com",
+    databaseURL: "https://portpolio-16967.firebaseio.com",
+    storageBucket: "portpolio-16967.appspot.com",
+    messagingSenderId: "54643699999"
+};
+
+firebase.initializeApp(config);
+
+ReactDOM.render(
+  <App />,
+  document.getElementById('root')
+);
