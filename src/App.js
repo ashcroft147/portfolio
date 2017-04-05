f05520c19fb91fb999b9fdda86c8aa29779e7e4a
ashcroft147 <pd050786@VDIPS.POSCO.DOM>
2017-04-05 18:40:40 +0900

    Update About Page

--- src/App.js
+++ src/App.js
@@ -9,7 +9,7 @@ class App extends Component {
     constructor(props) {
         super(props);
         this.state = {
-            urlList : "123"
+            urlList : ""
         }
         this._printDbInfo = this._printDbInfo.bind(this);
     }
--- src/components/about/About.js
+++ src/components/about/About.js
@@ -1,13 +1,22 @@
 import React, { Component } from 'react';
 import './About.css';
+import DoctorStrange from './img/doctor-strange.png';
 
 class About extends Component {
   render() {
     return (
 		<main>
 			<section id="about">
-				<div>
-					<h2>About Middle</h2>
+				<div className="container">
+					<div className='col-md-12 margin-top profile'>				
+						<img className="my_photo" src={DoctorStrange} alt="Kim Jeong-Hyun"></img>
+						<div className="wrapper big-content">
+							<h2>Kim JeongHyun Web Developer</h2>
+							<p>React, ES6, Saas, Webpack2, npm, Node.js</p>
+							<p>Love to development</p>
+							<p>Love to Commit at github</p>
+						</div>
+					</div>
 				</div>
 			</section>
 		</main>
